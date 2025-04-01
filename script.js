fd15c0e0c2f9eef10c85f2b240d10bb8167e7a9e
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 13:00:35 +0530

    this a code4

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
 
+               // this program is to demonstrate commenta in js
 
 
-
-console.log("this a new file ");
-
-
-/*
-this a mule line comment 
-for a code */
-
-console.log("this a to code use a commend line in the single line commend ");
+                console.log("this a new file ");
 
 
+                /*
+                this a mule line comment 
+                for a code */
+
+                console.log("this a to code use a commend line in the single line commend ");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-java enverfnment setup code editor choose note pad or any other text editor 
-
-
-
-*/
+               
