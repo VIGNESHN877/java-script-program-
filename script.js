cc868999664ae284f2395827e09dfe1978f4d7b5
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 15:29:08 +0530

    this a code4

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
  // variable declaration
 
+                
 
-
-            let bookname;
+            let bookname,bookauthor,bookyear;
             bookname = "Harry Potter and the Philosopher's Stone";
+            
+             bookauthor = "J.K. Rowling";
+             
+             bookyear = 1997;
+             // variable declaration end
+             // variable usage
 
-            console.log(bookname);
+             // variable initialization
+             console.log(bookname);
+             console.log(bookauthor);
+             console.log(bookyear);
+            
+
+             let bookname2 = "i love my self",bookauthor2 = "vignesh.n",bookyear2 = 2004, bookpublisher = "vignesh";
+             // variable initialization end
+             // variable usage
+             console.log(bookname2);
+             console.log(bookauthor2);
+             console.log(bookyear2);
+             console.log(bookpublisher);
+             // variable usage end
