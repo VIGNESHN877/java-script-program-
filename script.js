87a8049e96e05cf5900d2d865f14a55780c5aa73
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 15:57:34 +0530

    FIX:this a varable code change 1

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-            const name = "vignesh";
+const { log } = require("console");
+
+            const name1 = 1;
 
 
-          
+            const name = "vignesh.N";
+            const password = "vignesh123";
+            const email = "vignesh@gmail.com";
+         
+
+          console.log(name,password,email);
 
             console.log(name)
