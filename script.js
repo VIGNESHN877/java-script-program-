489da8186ea420e1cd55f62de8c824c2d07c47da
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 15:53:44 +0530

    FIX:this a varable code change 1

--- script.js
+++ script.js
@@ -1,58 +1,6 @@
- // variable declaration
-
-                
-               // do not start a number in a var and let and const
-
-               // keywords not allowed as variable names
-               // let, class, if, else, for, while, switch, case, break,
-               // continue, return, throw, try, catch, finally, super, this, void,
-               // with, yield, export, import, var, function, debugger, delete, in,
-               // instanceof, new, typeof, and, or, not, instanceof, equals, strict
-               // equality, strict inequality, greater than, less than, greater than or equal to, less
-               // than or equal to, bitwise AND, bitwise OR, bitwise XOR, bitwise NOT, left
-               // shift, right shift, left shift, right shift, unsigned right shift, unsigned left shift
-               // variable names can be any combination of letters, numbers, and underscores
-               // variable names cannot start with a number
-               // variable names cannot contain special characters
-               // variable names cannot be the same as a keyword
-               // variable names can be any length
-               // variable names can be any combination of letters, numbers, and underscores
-               // variable names cannot start with a number
-               // variable names cannot contain special characters
-               // variable names cannot be the same as a keyword
-               // variable names can be any length
-            let bookname,bookauthor,bookyear;
-            bookname = "Harry Potter and the Philosopher's Stone";
-            
-             bookauthor = "J.K. Rowling";
-             
-             bookyear = 1997;
-             // variable declaration end
-             // variable usage
-
-             // variable initialization
-             console.log(bookname,"\n",bookauthor,"\n",bookyear);
-            // var or using for a js 
-
-             let bookname2 = "i love my self",bookauthor2 = "vignesh.n",bookyear2 = 2004, bookpublisher = "vignesh";
-             // variable initialization end
-             // variable usage
-             console.log(bookname2,"\n",bookauthor2,"\n",bookyear2,"\n",bookpublisher);
-            
-             // variable usage end
+            const name = "vignesh";
 
 
-            var name = "vignesh";
+          
 
-             // varable other name is idenfiy
-
-            name = "rija tharik"; // change the value of name
-            name =bookauthor;
-
-
-            let one = "vikash";
-            one = "vikarm";
-
-            console.log(one);
-
-            console.log(name);
+            console.log(name)
