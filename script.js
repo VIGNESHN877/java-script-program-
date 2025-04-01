f57e18f2513543146cc6f08260f84b92086cbc9b
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 15:48:00 +0530

    FIX:this a varable code change 1

--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 
                 
                // do not start a number in a var and let and const
+
+               // keywords not allowed as variable names
+               // let, class, if, else, for, while, switch, case, break,
+               // continue, return, throw, try, catch, finally, super, this, void,
+               // with, yield, export, import, var, function, debugger, delete, in,
+               // instanceof, new, typeof, and, or, not, instanceof, equals, strict
+               // equality, strict inequality, greater than, less than, greater than or equal to, less
+               // than or equal to, bitwise AND, bitwise OR, bitwise XOR, bitwise NOT, left
+               // shift, right shift, left shift, right shift, unsigned right shift, unsigned left shift
+               // variable names can be any combination of letters, numbers, and underscores
+               // variable names cannot start with a number
+               // variable names cannot contain special characters
+               // variable names cannot be the same as a keyword
+               // variable names can be any length
+               // variable names can be any combination of letters, numbers, and underscores
+               // variable names cannot start with a number
+               // variable names cannot contain special characters
+               // variable names cannot be the same as a keyword
+               // variable names can be any length
             let bookname,bookauthor,bookyear;
             bookname = "Harry Potter and the Philosopher's Stone";
             
