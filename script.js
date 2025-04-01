09df701549fdd4ce1226a7a87ee577ece9a6b1ba
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 13:02:32 +0530

    this a code4

--- script.js
+++ script.js
@@ -4,12 +4,20 @@
 
                 console.log("this a new file ");
 
+              ()=>{
+            
+                var a = 10;
+                var b = 20;
+              }
+
+
+             console.log(a+b)
 
                 /*
                 this a mule line comment 
                 for a code */
 
-                console.log("this a to code use a commend line in the single line commend ");
+                console.log("this a to code use for a program");
 
 
                
