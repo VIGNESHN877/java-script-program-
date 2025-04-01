bb5d07041b2165f71a5de003be743117681aee62
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-01 16:24:19 +0530

    FIX:this a varable code change 2 and add for a colour types and date of birth and age update

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const { log } = require("console");
-
-            const name1 = 1;
+         const x = sum(2,3);
 
 
-            const name = "vignesh.N";
-            const password = "vignesh123";
-            const email = "vignesh@gmail.com";
+         const UNAME = "vigneshn877@";
+
+
+         const PWD = "THIS IS A FILE";
+
+         const WHITE_COLOUR ="#FFFF"
+         const BLACK_COLOUR = "#000000"
+         const RED_COLOUR = "#FF0000" 
+         const GREEN_COLOUR = "#00FF00"
+         const BLUE_COLOUR = "#0000FF"
+         const YELLOW_COLOUR = "#FFFF00"
+         const PURPLE_COLOUR = "#800080"
+         const ORANGE_COLOUR = "#FFA500"
+         const GREY_COLOUR = "#808080"
+         const CYAN_COLOUR = "#00FFFF"
+         const MAGENTA_COLOUR = "#FF00FF"
+         const LIGHTGREY_COLOUR = "#C0C0C0"
+         const LIGHTBLUE_COLOUR = "#ADD8E6"
+         const LIGHTGREEN_COLOUR = "#C6F4D6"
+         const LIGHTYELLOW_COLOUR = "#FFFFE0"
+         const LIGHTRED_COLOUR = "#FFC0CB"
+         const LIGHTPURPLE_COLOUR = "#C5C3C8"
+         const LIGHTORANGE_COLOUR = "#FFA07A"
+         const LIGHTCYAN_COLOUR = "#E0FFFF"
+         const LIGHTMAGENTA_COLOUR = "#FFC0CB"
          
 
-          console.log(name,password,email);
 
-            console.log(name)
+         const DATE_OF_BIRTH = '22.12.2004';
+
+         const age =calAge(DATE_OF_BIRTH);
+
+         
