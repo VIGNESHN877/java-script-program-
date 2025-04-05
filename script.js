374fcb68b5e8d9cc73ce9874d6add0dd28f26e0a
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-05 13:05:21 +0530

    this a java script training code is a type conversion code1

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+let age  = 23;
+
+console.log(typeof age); // Output: "number"
 
 
-// alert('this a alert');
+age = String(age);
 
-// console.log(alert);
-
-
+console.log(typeof age);
 
 
+let isadult = true;
+let isadult2 = 123;
+let isadult3 = null;
 
+
+
+isadult = String(isadult);
+
+console.log(isadult,isadult2,isadult3,);
+
+
+let num = '23';
+
+console.log(typeof num);
+
+
+ num =Number(num);
+
+console.log(num);
+
+console.log(typeof num);
