782e52bc86a76349d3841112ed4560b6743aecc5
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-05 10:36:55 +0530

    this a java script training code big int string types code

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ let intro =`da sollu da eena paraa' Hi "Hello, my name is vignesh.N"Nice to meet
 
 let channel = 'this a my channel ';
 
+
+let embed = `1+2 = ${1+2+(intro)}`;
+let eena = `i will be back ${"add",(intro)}`;
+
+
+
+
 console.log(
 
-
+eena ,"\n",
+embed,"\n",
 typeof intro,"\n",
 
-intro,"\n",
+intro,"\n",  
 
 intro+" and This is my friend rija tharik.M","\n",
 
