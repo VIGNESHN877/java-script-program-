9b4348a1620e49d5e87ab596c98519c38edc22d0
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-05 09:54:09 +0530

    this a java script training code big int data types biginit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let limit = 9007199254740991n;
 
-
+let one = 1234567890910n;
 console.log
 (
 
@@ -9,7 +9,8 @@ limit,
 typeof limit,"\n",
 limit + 1n,"\n",
 limit + 2n,"\n",
-limit + 3n,
-
+limit + 3n,"\n",
+limit + 4n,"\n",
+one + 2025, "\n",
 
 )
