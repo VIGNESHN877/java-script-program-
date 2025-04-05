af6c5e219e8a79a57c3354b19523b7080f1c7e5a
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-05 10:43:22 +0530

    this a java script training code Boolean data Types

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-// this a string data type
+let  condition1 = 5<10;
 
-
-let intro =`da sollu da eena paraa' Hi "Hello, my name is vignesh.N"Nice to meet you ' just try`;
-
-
-let channel = 'this a my channel ';
-
-
-let embed = `1+2 = ${1+2+(intro)}`;
-let eena = `i will be back ${"add",(intro)}`;
-
-
+for (let value=1; 1 < 10 ; value++)
 
 
 console.log(
 
-eena ,"\n",
-embed,"\n",
-typeof intro,"\n",
+value,'\n',
 
-intro,"\n",  
+    condition1,'\n',
 
-intro+" and This is my friend rija tharik.M","\n",
-
-channel,
-
-
-);
+    typeof condition1,'\n',
+)
