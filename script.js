82c3f7eb01be6bd58d0c08489f4592ef4f1c6958
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-05 09:27:43 +0530

    this a java script training code

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-         const x = sum(2,3);
+let pi = 3.14;
+
+let  prime = "29";
 
 
-         const UNAME = "vigneshn877@";
+console.log(pi,prime);
 
 
-         const PWD = "THIS IS A FILE";
-
-         const WHITE_COLOUR ="#FFFF"
-         const BLACK_COLOUR = "#000000"
-         const RED_COLOUR = "#FF0000" 
-         const GREEN_COLOUR = "#00FF00"
-         const BLUE_COLOUR = "#0000FF"
-         const YELLOW_COLOUR = "#FFFF00"
-         const PURPLE_COLOUR = "#800080"
-         const ORANGE_COLOUR = "#FFA500"
-         const GREY_COLOUR = "#808080"
-         const CYAN_COLOUR = "#00FFFF"
-         const MAGENTA_COLOUR = "#FF00FF"
-         const LIGHTGREY_COLOUR = "#C0C0C0"
-         const LIGHTBLUE_COLOUR = "#ADD8E6"
-         const LIGHTGREEN_COLOUR = "#C6F4D6"
-         const LIGHTYELLOW_COLOUR = "#FFFFE0"
-         const LIGHTRED_COLOUR = "#FFC0CB"
-         const LIGHTPURPLE_COLOUR = "#C5C3C8"
-         const LIGHTORANGE_COLOUR = "#FFA07A"
-         const LIGHTCYAN_COLOUR = "#E0FFFF"
-         const LIGHTMAGENTA_COLOUR = "#FFC0CB"
-         
+console.log(typeof pi);
+console.log(typeof prime);
+console.log(typeof pi,typeof prime);
 
 
-         const DATE_OF_BIRTH = '22.12.2004';
+// special numeric value 
 
-         const age =calAge(DATE_OF_BIRTH);
 
-         
+let infi = Infinity;
+
+let neginf = -Infinity;
+
+
+
+console.log(infi,"\n",neginf);
+
+
+console.log(typeof infi, "\n",typeof neginf);
