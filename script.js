2981cd36c2cd12510b23abd5981cdc182427843e
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-07 21:22:32 +0530

    this a java script training code is a type using for a increment and decrement code

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // increment and // Decrement 
 
 
-let x =23 ;
-x++;
-console.log(x); //24
+let x =12 ;
+
+console.log(x--); 
+
+console.log(x);
+
+console.log(++x); 
+console.log(++x); 
+
+console.log(x); 
