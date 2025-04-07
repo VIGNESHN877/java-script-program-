fbe968a3317b7c27854998df8da536f270ff254b
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-07 21:05:22 +0530

    this a java script training code is a type using for a binary code

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-let a = 20 , b = 20, c =20, d = 20, e = 20,
- f = 20, g = 20, h = 20,i = 20, j =20;
+let a = 12 , b= 2;
 
- let sum = a+b, sum2 = c-d, sum3 = e*f, sum4 = g/h, sum5 = i%j;
+let sum = a ** b ;
 
- console.log(sum,'\n',sum2,'\n',sum3,'\n',sum4,'\n',sum5,)
-
-
-// this a binary value and demo codeing 
+console.log(sum);
