1c81ca0a3a79552a251f16f138eb28e7b748057a
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-07 19:14:51 +0530

    this a java script training code is a type using for a string type and Number code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-    let age = '23';
-
-    age = age+10
-    console.log(age)
-    console.log(typeof age );
+             let age = '23.4';
 
 
-    age = Number(age);
-
-    
-
-    console.log(age+3);
+             console.log(age);
 
 
+             console.log(typeof age);
+
+
+             age = Number(age);
+
+
+             console.log(age+3);
