94dd95056ab53c2950ada0edefe124b29b185baf
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 10:52:19 +0530

    this a java script training code is a type using nested if leep yaer code  statement  in java script  code

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 let year = 2004;
 
+// divisible by 4 
+// divisible by 100, not divisible by 400
 
-if(year / 4 == 0 && year % 4 != 0) {
 
-    console.log('this a leep year')}
-    else if(year % 4 == 0 && year % 100 == 0 && year) {
+if (year % 4 == 0){
 
-        console.log('this a leep year')}
-        else{
-            console.log('this is not a leep year')
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            console.log(year+"  leap year");
         }
+        else{
+            console.log(year+'Not a leep year');
+        }
+    }
+    else{
+        console.log(year+" leap year");
+    }
+}
+else{ console.log('Not a leep year'); }
