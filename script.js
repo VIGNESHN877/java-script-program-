21df71ea92ab1f66a58274eedae64719374d283d
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 09:53:07 +0530

    this a java script training code is a type using for string comparisons and boolen data type  in java script  code

--- script.js
+++ script.js
@@ -3,13 +3,29 @@
 
 //console.log(null == undefined, null === undefined); // true
 
-console.log(undefined > 0);
-console.log(undefined<= 0);
-console.log(undefined< 0);
-console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined<= 0);
+// console.log(undefined< 0);
+// console.log(undefined == 0);
+// console.log(Number(undefined));
+
+// iN This code is 
 
 
 // console.log(  null > 0);
 // console.log( null<= 0);
 // console.log(null< 0);
 // console.log(null == 0);
+
+
+let a ='goms';
+
+let b = 'Zoms';
+
+let value = a >b ;
+let value2 = a === b;
+
+
+
+console.log(value,value2);
+
