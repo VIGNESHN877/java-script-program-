63e3ea890a7c3da379bd65afe69e7f976d1f4ad8
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 13:07:29 +0530

    this a java script training for factorail  loop while loop  in java script  code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // factorail
 
-let n =4 ;
+let n =5 ;
 
-let ans = 1 ;
+let ans = 0 ;
+let i = 1 ;
 
+while(i<=n){
 
-for(let i = 1;i<=n; i++){
-
-ans *= i ;
-console.log(ans);
+    ans +=i;
+    i++;
 }
+console.log(ans)
