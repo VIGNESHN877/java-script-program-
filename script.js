45eccb00795329d0ebb6de9df58a6ba254cd7826
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 11:34:47 +0530

    this a java script training logical opertarion in java script  code

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-
-let year = 2004;
-
-// divisible by 4 
-// divisible by 100, not divisible by 400
+let num = 11;
 
 
-if (year % 4 == 0){
+if (num %2 == 0) {
 
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
-            console.log(year+"  leap year");
-        }
-        else{
-            console.log(year+'Not a leep year');
-        }
-    }
-    else{
-        console.log(year+" leap year");
-    }
+    console.log('even number');
+
+
+
 }
-else{ console.log('Not a leep year'); }
+else{
+
+    console.log('odd Number ');
+
+}
