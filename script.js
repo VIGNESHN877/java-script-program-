af13478d547f1a2c4bdc574809beafb7c581dc02
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 12:52:10 +0530

    this a java script training loop  in java script  code

--- script.js
+++ script.js
@@ -4,13 +4,16 @@
 // printing from 1-10
 // initialisation,Validation, 
 
-let num =1 ;
+// let num =11 ;
 
-while (num < 10){
-   (num++)
-    console.log(num)
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <=10);
+// console.log('outside loop ')
 
+for(let num =1 ; num <=10 ; num++){
+
+
+    console.log('inside code block ' + num);
 }
-console.log('outside loop ')
-
-
