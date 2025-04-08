bd07ecf7a87c091f5c14cfbb1fb1c83cbf94109a
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 09:25:44 +0530

    this a java script training code is a type using for string comparisons in java script  code

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ let b = 'vigneshn';// -> true
 let result = a <b;
 
 let result1 = a > b;
+let result2 = a == b;
 
 console.log(result1)
 console.log(result);
+console.log(result2);
 // there are using unicode
