f2db6608c6bdb50d4bbc71c9b30ce3a52f9f0a56
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 10:22:01 +0530

    this a java script training code is a type using if and else statements  statement in java script  code

--- script.js
+++ script.js
@@ -4,20 +4,7 @@ let person;
 let age = 18 ;
 
 
-if(age >= 18, age <=50) {
-
-person = "the age is greater than 18";
-}
-
-else if(age > 50, age < 100)
-    { 
-    person = "the age is greater than 51";
-}
-else{
-
-    person = ' the age is less than 18';
-}
+person = (age > 18) ? "adult" : "minor";
 
 
-
-console.log(person);
+console.log(' '+person);
