ab382bd7952cd88731e9b5dbf367f47a7644d435
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-08 10:38:50 +0530

    this a java script training code is a type using if and else  statement demo code statement  in java script  code

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let person;
+let x = 10;
 
 
-let age = 51 ;
-if ('0'){
-    console.log('0 is a string');
+if (x > 0){
+
+    console.log('passitive')
+} 
+else if (x < 0) {
+
+    console.log('nagative')
+
+}
+else {
+    console.log('zero')
 }
