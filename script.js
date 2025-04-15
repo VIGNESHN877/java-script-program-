c12fdeede3e416d26fc17c6c53d46a6be4087146
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-15 15:04:53 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-let X5 = (n)=> 5*n;
+// the next is object programming code
 
-console.log(X5(6))
+let car={
+
+name :'Nexon',
+model :'2020',
+color :'Black',
+price : 1500000,
+speed : 120,
+
+
+
+};
+
+console.log(car.color);
+
+let car2={
+
+   name1:'vin',
+   module:'2020',
+   color1:'red',
+   price1: 1500000,
+   speed1: 120,
+}
+
+console.log(car2.price1);
