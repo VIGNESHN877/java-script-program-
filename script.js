01f86573589ee3d59e309a41108896fc6878ae49
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-15 15:51:51 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// the next is object programming code
-
-let car={
-
-name :'Nexon',
-model :'2020',
-color :'Black',
-price : 1500000,
-speed : 120,
 
 
 
+      let car ={
+         name:'nexon',
+         color:'black',
+         price:2000000
+         }
+         console.log(car);
 
-};
-
-console.log(car.color);
-
-let car2={
-
-   name1:'vin',
-   module:'2020',
-   color1:'red',
-   price1: 1500000,
-   speed1: 120,
-}
-
-console.log(car2.price1);
+      
