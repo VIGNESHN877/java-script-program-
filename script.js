46acd7b5e357d304b23209bfd90b475638c5166b
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-15 11:54:37 +0530

    this a java script training for in java script using for a  function  in code

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
- let a = 13 , b = 24;
+function sum(a,b){
 
-
-// console.log((a+b/a)*b);
-
-
-function solve(a,b) {
-   console.log((a+b/b)*b);
+   console.log('\n',a+b,'\n',a-b,'\n',a*b,'\n',a/b,'\n',a%b);
+   
 }
 
-solve(a,b);
-
-solve(2002,2004);
+sum(230,200);
