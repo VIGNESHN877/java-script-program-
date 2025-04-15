fabf77edda68b3b51ebec8ca88187aff220ebb85
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-15 14:41:09 +0530

    this a java script training for in java script using for a  function  in code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let operation = (op,a,b) => op(a,b);
-let add = (a,b) => a+b;
-let sub = (a,b) => a-b;
-let divi = (a,b) => a/b;
-let rem = (a,b) => a-b;
+let X5 = (n)=> 5*n;
 
-console.log(operation(sub,21,3),'\n',(add,40,50),'\n',(divi,50,3),'\n',(rem,50,4));
+console.log(X5(6))
