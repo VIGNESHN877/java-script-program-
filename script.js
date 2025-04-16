0e5661779f9055edf390e6142efd8a365c93f869
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 09:46:30 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-    let details = 'this a car  hart of the oil '
-    let car ={
-         name:'nexon',
-         brand:'tata',
-         "drive train":details+'petrol,diesal,ev',
-         price:15,
-         color:'red,blue,green',
-         mileage:20,
-         onroadprice(price){
-            return this.price+2;
-         }
-   }
+let user ={
+   name: "vignesh",
+   age:20,
+};
 
-    console.log(car.onroadprice(18));
-   //  console.log(car); // this line print line
-   //  console.log(car);
-   // console.log(car.price); // this link print for price only print
-   // console.log(car["drive train"]); // this link print for drive train only print
-   // car['modelYear']=2025; //this line add new key value pair
-   // delete car.price;// this line delete key value pair
-   // console.log(car); // this line print all key value pair
-   // car['price']=2000; // this line add new key value pair
-   // console.log(car); // this line print all key value pair
-   // console.log (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`); // this a link type a sendence for a value of key
+
+console.log(user.name);
+user.name ='vignesh N'
+console.log(user);
+console.log(user.name);
