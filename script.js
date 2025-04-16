2b43f463eb090b326006a27df8deb7e77622d9c8
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 10:27:22 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -7,13 +7,17 @@
      
      function User(a){
       this.name= 'vignesh';   // this is used to access the properties of the object
-      }
+     this.age=function(){
+      return 25;
+     }
+   }
       let vignesh = new User;
       // let rija = new User();
  
       
 
       // console.log(rija);
-      console.log(vignesh);
+      // console.log(vignesh);
+      console.log(vignesh.age());
 
       // consuter
