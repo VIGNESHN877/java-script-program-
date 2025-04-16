b966b908d0e4a46f0cce16c74894a99a95de0af5
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 11:36:49 +0530

    this a java script training in using for a Array  code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Array 
 // array other modthod 
 
-let veges =['carrot','beetroot ','pumkin',true,null ,{name:'vignesh',age:20}, undefined,23]; // 5 empty slots
+let veges =['carrot','beetroot ','pumkin',true,null ,{name:'vignesh',age:20}, undefined,23,function add(a,b){return a+b}]; // 5 empty slots
 
-console.log(veges[5].age);
+console.log(veges[8](2,5));
 
