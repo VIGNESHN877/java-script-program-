d924ebadbd33a3edd66bbff4c0c3b9eff5ef4e0b
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 14:24:10 +0530

    this a java script training in using for a Javascript program recursive call code

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// Date and Time
+// recursive function to find the maximum value in the array
+// -BASE CASE: if the array has only one element, return that element
+// -UPDATE CASE: if the array has more than one element, find the maximum value between the first element and
+// the maximum value of the rest of the array
+// -RECURSIVE CALL
 
+const { cachedDataVersionTag } = require("node:v8");
 
-let date = new Date('2004-12-22');
+function factorial(n){
+    return n*factorial(n-1);
 
-
-
-
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-let ampm = hours >= 12 ? 'PM' : 'AM';
-hours = hours % 12;
-hours = hours ? hours : 12; // display '12' instead of '0'
-minutes = minutes < 10 ? '0' + minutes : minutes;
-seconds = seconds < 10 ? '0' + seconds : seconds;
-// Display the date and time in the specified format
-console.log(`${hours}:${minutes}:${seconds} ${ampm}`);
-// Output: 12:30:45 PM  
+}
+// 5! => 0+1+2+3+4+5=15
