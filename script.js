bb0488f8aa089e47c0ec55ddaa9461684c2545e9
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 09:25:15 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
     let car ={
          name:'nexon',
          brand:'tata',
-         drivetrain:details+'petrol,diesal,ev',
+         "drive train":details+'petrol,diesal,ev',
          price:5000000,
          color:'red,blue,green',
          mileage:20
    }
 
-
-   console.log(car.price);
-   console.log(car.drivetrain);
-   // (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`);
+   console.log(car); // this line print line
+   console.log(car.price); // this link print for price only print
+   console.log(car["drive train"]); // this link print for drive train only print
+   car['modelYear']=2025; //this line add new key value pair
+   delete car.price;// this line delete key value pair
+   console.log(car); // this line print all key value pair
+   car['price']=2000; // this line add new key value pair
+   console.log(car); // this line print all key value pair
+   console.log (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`); // this a link type a sendence for a value of key
