1c2ecef0f23add713cd6d3badd50cfe1074aa777
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 12:48:17 +0530

    this a java script training in using for a Javascript program file DATE AND TIME code

--- script.js
+++ script.js
@@ -4,24 +4,16 @@
 let date = new Date('2004-12-22');
 
 
- let year = date.getFullYear();
- let month = date.getMonth() + 1;
- let day = date.getDate();
- console.log(year, month, day); // Output: 2004 12 22
-// // Output: 2004 12 22
 
 
-
-console.log(date);
-
-// let hours = date.getHours();
-// let minutes = date.getMinutes();
-// let seconds = date.getSeconds();
-// let ampm = hours >= 12 ? 'PM' : 'AM';
-// hours = hours % 12;
-// hours = hours ? hours : 12; // display '12' instead of '0'
-// minutes = minutes < 10 ? '0' + minutes : minutes;
-// seconds = seconds < 10 ? '0' + seconds : seconds;
-// // Display the date and time in the specified format
-// console.log(`${hours}:${minutes}:${seconds} ${ampm}`);
-// // Output: 12:30:45 PM  
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+hours = hours % 12;
+hours = hours ? hours : 12; // display '12' instead of '0'
+minutes = minutes < 10 ? '0' + minutes : minutes;
+seconds = seconds < 10 ? '0' + seconds : seconds;
+// Display the date and time in the specified format
+console.log(`${hours}:${minutes}:${seconds} ${ampm}`);
+// Output: 12:30:45 PM  
