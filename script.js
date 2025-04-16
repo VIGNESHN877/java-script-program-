aa91f6a1ad891eb4f5bf0efe365aba1e4af6f30d
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 09:17:06 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-      let car ={
+    let details = 'this a car  hart of the oil '
+    let car ={
          name:'nexon',
          brand:'tata',
-         drivetrain:'petrol,diesal,ev',
+         drivetrain:details+'petrol,diesal,ev',
          price:5000000,
          color:'red,blue,green',
          mileage:20
    }
 
 
-   console.log
-   (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`)
+   console.log(car.price);
+   console.log(car.drivetrain);
+   // (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`);
