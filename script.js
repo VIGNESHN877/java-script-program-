f03f67ad6b3b9fe8bc3283493b3da6d1b0a88f1d
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 09:39:14 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
          name:'nexon',
          brand:'tata',
          "drive train":details+'petrol,diesal,ev',
-         price:5000000,
+         price:15,
          color:'red,blue,green',
-         mileage:20
+         mileage:20,
+         onroadprice(price){
+            return this.price+2;
+         }
    }
 
-   console.log(car); // this line print line
-   console.log(car.price); // this link print for price only print
-   console.log(car["drive train"]); // this link print for drive train only print
-   car['modelYear']=2025; //this line add new key value pair
-   delete car.price;// this line delete key value pair
-   console.log(car); // this line print all key value pair
-   car['price']=2000; // this line add new key value pair
-   console.log(car); // this line print all key value pair
-   console.log (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`); // this a link type a sendence for a value of key
+    console.log(car.onroadprice(18));
+   //  console.log(car); // this line print line
+   //  console.log(car);
+   // console.log(car.price); // this link print for price only print
+   // console.log(car["drive train"]); // this link print for drive train only print
+   // car['modelYear']=2025; //this line add new key value pair
+   // delete car.price;// this line delete key value pair
+   // console.log(car); // this line print all key value pair
+   // car['price']=2000; // this line add new key value pair
+   // console.log(car); // this line print all key value pair
+   // console.log (`${car.name} is a ${car.brand} car with a ${car.drivetrain}`); // this a link type a sendence for a value of key
