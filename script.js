7de0f12ae141568de6019a972e74311ce6b6b04d
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 12:48:01 +0530

    this a java script training in using for a Javascript program file DATE AND TIME code

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// JSON - JavaScript Object Notation
+// Date and Time
 
 
-let user ='{"name": "vignesh","age ": 25,"city": "chennai"}'
+let date = new Date('2004-12-22');
 
 
-console.log(JSON.parse(user));
+ let year = date.getFullYear();
+ let month = date.getMonth() + 1;
+ let day = date.getDate();
+ console.log(year, month, day); // Output: 2004 12 22
+// // Output: 2004 12 22
+
+
+
+console.log(date);
+
+// let hours = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// let ampm = hours >= 12 ? 'PM' : 'AM';
+// hours = hours % 12;
+// hours = hours ? hours : 12; // display '12' instead of '0'
+// minutes = minutes < 10 ? '0' + minutes : minutes;
+// seconds = seconds < 10 ? '0' + seconds : seconds;
+// // Display the date and time in the specified format
+// console.log(`${hours}:${minutes}:${seconds} ${ampm}`);
+// // Output: 12:30:45 PM  
