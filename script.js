73f465823aa9fa20bcfa266dbd5767363575bace
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 09:53:12 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ let user ={
 // in opertor 
 
 console.log("age" in user); // true
+console.log("name" in user); // true
+console.log("city" in user); // false
+
+for(key in user){
+
+   console.log(user[key],[key]);
+//    console.log(key+'-'+user[key]);
+ }
