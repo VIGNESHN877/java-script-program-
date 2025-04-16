2d1a8757b37060f75536e078dac919edcfad24ff
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 11:46:04 +0530

    this a java script training in using for a Array  code

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 
 let veges =['carrot','beetroot ','pumkin'];
 
-for(veges of veges){
+for(vege in  veges){
 
-   console.log(veges);
-}
+   console.log(veges[vege]);
+}// of operator is used to iterate over the array elements
+// in operator is used to check if the element is present in the array or not
