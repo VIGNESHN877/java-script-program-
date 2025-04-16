47610ad3f0c7289b66f3fd9da14a4a2e4f29c85d
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 11:36:59 +0530

    this a java script training in using for a Array  code

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 
 let veges =['carrot','beetroot ','pumkin',true,null ,{name:'vignesh',age:20}, undefined,23,function add(a,b){return a+b}]; // 5 empty slots
 
-console.log(veges[8](2,5));
+console.log(veges[8](2,5)); // 7
 
