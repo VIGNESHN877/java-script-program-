38a8d6d1b6b2f2ab167183ef5e6270d0de7f5c9a
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-16 10:04:47 +0530

    this a java script training for in java script using for a object  in code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let a ={
+const a ={
 
 name: "vignesh",
 age: 20,
 };
 
-let b = a ;
 
-console.log(a,b);
 
-b= {};
+console.log(a);
 
-b.age= 21;
+// a= {}; // This will throw an error because a is a const variable
 
-console.log(a,b);
+a.age= 21;
+
+console.log(a);
