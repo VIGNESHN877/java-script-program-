c014124611e6de856388c4f66f583e695b1e1612
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-25 09:44:03 +0530

    this object code

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ school:"G.H.S.SCHOOL",
 // this a object code file 
 console.log(typeof(x),(x),(x.name));
 
-
+let two = "player";
 
 let y = {
 
     name1:"vignesh",
-    work:"full stack development",
+    work:"full stack development + " + two ,
     current:"student",
 
 }
@@ -21,3 +21,6 @@ console.log(
     (y.name1),
     y  
 )
+
+
+console.log(y.work)
