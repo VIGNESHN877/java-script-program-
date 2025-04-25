7bceaa3baa81a3f3ebca1b3412f0e1682e706042
VIGNESHN877 <vigneshn877@gmail.com>
2025-04-25 09:33:14 +0530

    this object code

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-        let  user = {  // object
-            name: 'John Doe',
-            age: 30,
-            occupation: 'Software Engineer',
-           
-        };
+let x ={
+name:"vignesh",
+class: "12th pass",
+school:"G.H.S.SCHOOL",
 
+};
 
-        // class 
-        class User {
-        
-            name = 'John Doe';
-            age = 30;  
-            display(){
-                console.log(`Name: ${this.name}, Age: ${this.age}`);
-            } 
-            //   constructor(name, age) {
-            //     this.name = name;
-            //     this.age = age;
-            //   }
-        }
-       let user1 = new User();
-       user1.display();
-       console.log(user1);
+console.log(typeof(x),(x),(x.name))
